6811f6460daab6f8bf626d3d4909932aa51d2aec
RossiGu <gustavo.2017316329@gmail.com>
2023-09-04 19:36:56 -0300

    finish project

---
+++ src/main/java/com/blog/myblog/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.blog.myblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] AUTH_LIST = {
+            "/",
+            "/posts",
+            "/posts/{id}"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((AUTH_LIST) -> AUTH_LIST
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/com/blog/myblog/controllers/MyblogController.java
+++ src/main/java/com/blog/myblog/controllers/MyblogController.java
@@ -2,11 +2,17 @@ package com.blog.myblog.controllers;
 
 import com.blog.myblog.models.Posts;
 import com.blog.myblog.servicies.MyblogService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -21,5 +27,29 @@ public class MyblogController {
         List<Posts> posts = myblogService.findAll();
         mv.addObject("posts", posts);
         return mv;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Posts posts = myblogService.findById(id);
+        mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @GetMapping("/newpost")
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @PostMapping("/newpost")
+    public String savePost(@Valid Posts posts, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique os campos obrigatórios!");
+            return "redirect:/newpost";
+        }
+        posts.setDate(LocalDate.now());
+        myblogService.save(posts);
+        return "redirect:/posts";
     }
 }
